a772cbc592486465ff14df9f265d8f845fbeab28
rajpatel <rajpatel8726@gmail.com>
2025-02-05 18:16:16 +0530

    add

---
+++ pages.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+
+<head>
+
+
+<meta charset="utf-8">
+
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+<title>Expert Interior Designers in Lucknow – Stunning Makeovers for Every Budget</title>
+<meta name="description" content="Looking for the best interior designers in Lucknow? We offer expert home, office, and commercial interior solutions with modern designs and customized decor.">
+<meta name="keywords" content="interior designers in Lucknow, best interior design in Lucknow, home interior Lucknow, office interior Lucknow, luxury interior design, modular kitchen design, custom furniture Lucknow, turnkey interior solutions">
+<meta name="author" content="Thewoodsinterior">
+
+<?php include '_header.php'; ?>
+
+</head>
+
+<body>
+
+
+<?php include '_menu.php'; ?>
+
+<section id="hero" class="d-flex align-items-center" style="background-image: url(assets/img/interior/hero-interior.webp); background-size: cover; background-position: center; background-attachment: fixed;">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-lg-8 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+
+<h1 style="font-family: Montserrat, sans-serif; text-transform: none;">Expert Interior Designers in Lucknow – Stunning Makeovers for Every Budget</h1>
+
+<h2>Experience elegant, functional, and modern interiors in Lucknow, designed to reflect your unique style</h2>
+
+
+
+<div class="banner_btn" style="margin-top:20px;">
+
+<a href="tel:+919140012929" class="call btn btn-primary"><i class="fa-solid fa-phone"></i>&nbsp;+91 9140012929 </a>
+
+</div>
+
+</div>
+
+<div class="col-lg-4 order-1 order-lg-2 hero-img">
+
+<div class="side_form mt-5">
+
+<div class="contact-from-wrap primary-bg">
+
+
+
+
+<div class="form_header text-center">
+
+<img src="assets/img/interior/goverment-proof.png" alt="" class="img-fluid logogold">
+<div class="text-center">
+<img src="assets/img/interior/mainlogo.png" alt="Logo">
+
+</div>
+
+</div>
+<div style="font-size: 24px; font-weight: bold; padding: 10px; background: linear-gradient(90deg, rgb(11, 54, 196), #2ec650); -webkit-background-clip: text; -webkit-text-fill-color: transparent; text-align: center;">
+Call Or Whatsapp Us For Free Home Demo
+</div>
+
+
+
+<form method="POST" name="indexform" action="enquiry.php" class="contact-from">
+
+
+
+<div class="input-field">
+
+<div class="inputs"><i class="fa fa-user" aria-hidden="true"></i><input type="text" class="form-control" name="name" required="" placeholder="Name" tabindex="0"> </div>
+
+
+
+</div>
+
+
+
+<div class="input-field">
+
+
+
+<div class="inputs"><i class="fa fa-phone" aria-hidden="true"></i><input type="text" class="form-control" name="telphone" required="" placeholder="Mobile" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" maxlength="10" pattern="[1-9]{1}[0-9]{9}" required tabindex="0"></div> 
+
+
+
+</div>
+
+
+
+<div class="input-field">
+
+<div class="inputs">
+
+<i class="fa fa-users" aria-hidden="true"></i>
+
+<select name="people" required="" class="form-control" tabindex="0">
+
+<option value="0">Services</option>
+
+<option value="1">Residential Interior</option>
+
+<option value="2">Commercial Interior</option>
+
+<option value="3">Custom Furniture & Decor</option>
+
+<option value="4">Turnkey Interior</option>
+
+<option value="5">3D Interior Designing & Consultation</option>
+
+
+
+</select>
+
+</div>
+
+</div>
+
+
+
+<div class="input-field">
+
+  <div class="inputs">
+
+  <i class="fa fa-calendar" aria-hidden="true"></i><input class="form-control" placeholder="Booking Date" class="textbox-n" type="text" onfocus="(this.type='date')" onblur="(this.value == '' ? this.type='text' : this.type='date')" autocomplete="off" name="arrdate" required/>
+
+  </div>
+
+  
+
+</div>
+
+
+
+<p class="text-center">
+
+
+
+<button type="submit" name="send_message_btn" class="btn btn-primary twoToneButton" value="ENQUIRY NOW" tabindex="0">Send Enquiry</button>
+
+
+
+</p>
+
+
+
+</form>
+
+
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<div class="overlay"></div>
+
+</section>
+
+
+<section>
+
+<div class="cta-container text-center py-5 mb-0">
+    <div class="cta-overlay"></div> <!-- Semi-transparent overlay -->
+    <div class="cta-content">
+        <h2 class="cta-heading">Call Us for a Free Home Demo!</h2>
+        <p class="cta-description">Get a personalized, no-cost demo of your future home. Call now to book your free consultation.</p>
+        <a href="tel:+919140012929" class="cta-button call btn btn-primary">Call Now</a>
+    </div>
+</div>
+
+
+<div class="container p-4">
+    <h2 class="text-center">Our Pages</h2>
+    <div class="col-md-3">
+        <a href="deoria.php">Best Interior Design in Deoria </a> <br>
+        <a href="gorakhpur.php">Best Interior Design in Gorakhpur </a>
+
+    </div>
+    <div class="col-md-3"></div>
+    <div class="col-md-3"></div>
+    <div class="col-md-3"></div>
+</div>
+
+
+
+<?php include '_footer.php';?>
+
+
+
+</body>
+
+
+</html>
