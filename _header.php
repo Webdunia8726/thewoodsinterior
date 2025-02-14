0e3c5a58f49979a88e38bd3264320c3f36557b35
rajpatel <rajpatel8726@gmail.com>
2025-02-14 11:16:41 +0530

    add

--- _header.php
+++ _header.php
@@ -22,37 +22,3 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
   <link href="assets/css/carpenter.css" rel="stylesheet">
 
-
-<!-- Google tag (gtag.js) -->
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11306456478"></script>
-
-<script>
-
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag(){dataLayer.push(arguments);}
-
-  gtag('js', new Date());
-
-
-
-  gtag('config', 'AW-11306456478');
-
-</script>
-
-<!-- Event snippet for WhatsappButton conversion page -->
-
-<script>
-
-  gtag('event', 'conversion', {'send_to': 'AW-11306456478/CD8rCJPE29YYEJ6rq48q'});
-
-</script>
-
-<!-- Event snippet for Leads Form conversion page -->
-
-<script>
-
-  gtag('event', 'conversion', {'send_to': 'AW-11306456478/lAlFCJbE29YYEJ6rq48q'});
-
-</script>
